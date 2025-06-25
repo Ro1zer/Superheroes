3c36abde61cba78345a71b46103a4449b5f5ce3a
Maksym Oleksiienko <starsredmaks@gmail.com>
2025-06-25 19:52:23 +0300

    Add small code refactor after comparing with original solve

--- app/src/main/java/com/example/superheroes/MainActivity.kt
+++ app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -5,17 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.superheroes.model.HeroesRepository
 import com.example.superheroes.ui.theme.SuperheroesTheme
-import com.example.superheroes.view.HeroesScreen
+import com.example.superheroes.view.HeroesListItem
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,16 +26,28 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SuperheroesTheme {
-                Scaffold(
+                Surface(
                     modifier = Modifier.fillMaxSize(),
-                    topBar = {
-                        SuperheroesTopAppBar()
-                    }
-                ) { innerPadding ->
-                    HeroesScreen(Modifier.padding(innerPadding))
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    SuperheroesApp()
                 }
             }
         }
+    }
+}
+@Composable
+fun SuperheroesApp(){
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            SuperheroesTopAppBar()
+        }
+    ) { innerPadding ->
+        HeroesListItem(
+            heroes = HeroesRepository.heroes,
+            contentPadding = innerPadding
+        )
     }
 }
 
@@ -48,4 +63,21 @@ fun SuperheroesTopAppBar(modifier: Modifier = Modifier) {
         },
         modifier = modifier
     )
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    device = Devices.PHONE
+)
+@Composable
+fun SuperheroesAppPreview(){
+    SuperheroesTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            SuperheroesApp()
+        }
+    }
 }
--- app/src/main/java/com/example/superheroes/view/HeroesScreen.kt
+++ app/src/main/java/com/example/superheroes/view/HeroesScreen.kt
@@ -5,21 +5,27 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -27,21 +33,24 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.superheroes.R
 import com.example.superheroes.model.Hero
-import com.example.superheroes.model.HeroesRepository
 import com.example.superheroes.ui.theme.SuperheroesTheme
 
 @Composable
-fun HeroesScreen(modifier: Modifier = Modifier) {
+fun HeroesListItem(
+    heroes: List<Hero>,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp)
+) {
     LazyColumn(
         modifier = modifier,
+        contentPadding = contentPadding
     ) {
-        items(HeroesRepository.heroes) {
-            HeroCard(
+        items(heroes) {
+            HeroCardItem(
                 hero = it,
                 modifier = Modifier.padding(
-                    start = dimensionResource(R.dimen.padding_large),
-                    end = dimensionResource(R.dimen.padding_large),
-                    bottom = dimensionResource(R.dimen.padding_medium)
+                    horizontal = dimensionResource(R.dimen.padding_large),
+                    vertical = dimensionResource(R.dimen.padding_medium)
                 )
             )
         }
@@ -49,19 +58,20 @@ fun HeroesScreen(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun HeroCard(
+fun HeroCardItem(
     hero: Hero,
     modifier: Modifier = Modifier
 ) {
     Card(
         elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
-        shape = MaterialTheme.shapes.medium,
+        shape = RoundedCornerShape(dimensionResource(R.dimen.padding_large)),
         modifier = modifier
     ) {
         Row(
             modifier = Modifier
+                .fillMaxWidth()
                 .padding(dimensionResource(R.dimen.padding_large))
-                .height(dimensionResource(R.dimen.size_large)),
+                .sizeIn(minHeight = dimensionResource(R.dimen.size_large)),
         ) {
             HeroInformation(
                 name = hero.nameId,
@@ -71,6 +81,9 @@ fun HeroCard(
             Spacer(Modifier.width(dimensionResource(R.dimen.padding_medium)))
             HeroImage(
                 image = hero.imageId,
+                modifier = Modifier
+                    .size(dimensionResource(R.dimen.size_large))
+                    .clip(RoundedCornerShape(dimensionResource(R.dimen.padding_medium)))
             )
         }
     }
@@ -87,9 +100,8 @@ fun HeroImage(
         Image(
             painter = painterResource(image),
             contentDescription = null,
-            modifier = Modifier
-                .size(dimensionResource(R.dimen.size_large))
-                .clip(shape = MaterialTheme.shapes.small)
+            alignment = Alignment.TopCenter,
+            contentScale = ContentScale.FillWidth
         )
     }
 }
@@ -106,8 +118,8 @@ fun HeroInformation(
         Text(
             text = stringResource(name),
             style = MaterialTheme.typography.displaySmall,
-            modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_small)),
         )
+        Spacer(Modifier.height(dimensionResource(R.dimen.padding_small)))
         Text(
             text = stringResource(description),
             style = MaterialTheme.typography.bodyLarge,
@@ -117,10 +129,14 @@ fun HeroInformation(
 
 @Preview
 @Composable
-fun HeroCardPreview() {
+fun HeroCardItemPreview() {
     SuperheroesTheme {
-        HeroCard(
-            hero = HeroesRepository.heroes[0]
+        HeroCardItem(
+            hero = Hero(
+                R.string.hero1,
+                R.string.description1,
+                R.drawable.android_superhero1
+            )
         )
     }
 }
